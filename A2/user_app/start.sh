cdba22b79cdefffa8a1c810ec441a48525c3db03
SHI HU <shihu@SHIs-MacBook-Pro.local>
2019-11-07 18:24:43 -0500

    Add gunicorn log file

--- A2/user_app/start.sh
+++ A2/user_app/start.sh
@@ -1,1 +1,1 @@
-/home/ubuntu/venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 --chdir /home/ubuntu/Desktop/ece1779_lab/A2/user_app app:webapp
+/home/ubuntu/venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 --access-logfile /home/ubuntu/Desktop/gunicorn.log --chdir /home/ubuntu/Desktop/ece1779_lab/A2/user_app app:webapp

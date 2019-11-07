2b14858f9942b1a69a8e965218d165c955c5c05e
SHI HU <shihu@SHIs-MacBook-Pro.local>
2019-11-07 17:01:49 -0500

    Modify user-app start.sh

--- A2/user_app/start.sh
+++ A2/user_app/start.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:5000 --workers=1 --chdir /home/ubuntu/Desktop/ece1779_lab/A2/user_app app:webapp
+/home/ubuntu/.local/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 --chdir /home/ubuntu/Desktop/ece1779_lab/A2/user_app app:webapp

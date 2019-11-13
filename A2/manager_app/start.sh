7a73ae283f25de4186608dd2e71c129a071076a5
SHI HU <shihu@SHIs-MacBook-Pro.local>
2019-11-13 18:34:52 -0500

    Try a new start.sh

--- A2/manager_app/start.sh
+++ A2/manager_app/start.sh
@@ -1,4 +1,5 @@
 . /home/ubuntu/venv/bin/activate
 alias proj="cd /home/ubuntu/Desktop/ece1779_lab/A2/manager_app/"
 python3 /home/ubuntu/Desktop/ece1779_lab/A2/manager_app/run.py &
+alias proj="cd /home/ubuntu/Desktop/ece1779_lab/A2/manager_app/app/auto-scaler/"
 python3 /home/ubuntu/Desktop/ece1779_lab/A2/manager_app/app/auto-scaler/auto_scaler.py
